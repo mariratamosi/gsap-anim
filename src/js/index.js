e81e3bf3717fd9ff77651d13d9c45611b49ca12b
marira <rongrazzo@gmail.com>
2021-06-30 01:57:05 +0600

    init anim

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+import gsap from "gsap";
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("DOM fully loaded and parsed");
+  gsap.from(".content", {
+    opacity: 0,
+    duration: 1,
+    y: -50,
+  });
+});
